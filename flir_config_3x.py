1190a82acbb56e507a05433b324df70257b62bc1
jb2849 <junchibin@outlook.com>
2022-09-12 23:59:10 -0700

    Update flir_config_3x.py

--- flir_config_3x.py
+++ flir_config_3x.py
@@ -184,8 +184,8 @@ train.ddp.fp16_compression = True
 train.init_checkpoint = ""
 train.max_iter = 270000
 train.output_dir = '/content/drive/MyDrive/mvitv2Debug'
-train.checkpointer=dict(period=500, max_to_keep=100) # options for PeriodicCheckpointer
-train.eval_period=500
+train.checkpointer=dict(period=5000, max_to_keep=100) # options for PeriodicCheckpointer
+train.eval_period=5000
 
 
 lr_multiplier = L(WarmupParamScheduler)(
